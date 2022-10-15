0c7c675bce8e47c1aab2c317ad52823a0dc91445
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-15 19:32:35 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿// Chertousova PE
+// Task 3 DZ
+// 15.10.2022
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b, c, D;
+
+	cin >> a >> b >> c;
+
+	D = b * b - 4 * a * c;
+
+	if (D > 0) {
+		cout << "2";
+	}
+	if (D == 0) {
+		cout << "1";
+	}
+	if (D < 0) {
+		cout << "0";
+	}
+
+}
